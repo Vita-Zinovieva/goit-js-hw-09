6bd15f275cbad394710abfa8b21c1ff168e6c4f6
Vita <ekokalgan@gmail.com>
2023-02-18 20:08:49 +0200

    !!!

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,17 +1,17 @@
-const startButtob = document.querySelector('button[data-start = "Start"]');
-const stoptButtob = document.querySelector('button[data-stop = "Stop"]');
+const startBtn = document.querySelector('button[data-start = "Start"]');
+const stoptBtn = document.querySelector('button[data-stop = "Stop"]');
 const body = document.querySelector('body');
 
 
 //ставимо прослуховувач на кнопку старт
-startButtob.addEventListener(clic, () => {
+startBtn.addEventListener(clic, () => {
     let timerId = setInterval(() => {           //таймер виконання функції зміни кольору
 body.style.backgroundColor = getRandomHexColor;
 }, 1000);
 })
 
 //ставимо прослуховувач на кнопку стоп
-stoptButtob.addEventListener(click, () => {
+stoptBtn.addEventListener(click, () => {
     clearInterval(timerId);      //метод зупинки функції   
 })
 
