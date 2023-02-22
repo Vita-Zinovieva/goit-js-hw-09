3b64c82134507a6596a82cb739443c713f06015e
Vita <ekokalgan@gmail.com>
2023-02-22 14:00:24 +0200

    02.js-done

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,28 +1,19 @@
-// Описаний в документації
-import flatpickr from "flatpickr";
-// Додатковий імпорт стилів
-import "flatpickr/dist/flatpickr.min.css";
-import Notiflix from 'notiflix';
 
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const startBtn = document.querySelector('button[data-start]');
-const timerFace = document.querySelector('.timer');
 const dateInput = document.querySelector('input#datetime-picker');
 const daysIndicator = document.querySelector('[data-days]');
 const hoursIndicator = document.querySelector('[data-hours]');
 const minutesIndicator = document.querySelector('[data-minutes]');
 const secondsIndicator = document.querySelector('[data-seconds]');
 
-
-function createTimerFace({ days, hours, minutes, seconds }) {
-    daysIndicator.textContent = days;
-    hoursIndicator.textContent = hours;
-    minutesIndicator.textContent = minutes;
-    secondsIndicator.textContent = seconds;
-}
-
-//функція створення екземпляру дати та часу
-flatpickr(dateInput, options);
+startBtn.disable = true;
+let currentDate = null;
+let selectedDate = null;
+let timerId = null;
 
 //створений об'єкт з завданя
 const options = {
@@ -31,63 +22,63 @@ const options = {
     defaultDate: new Date(),//Встановлює початкові вибрані дати.
     minuteIncrement: 1,   //Регулює крок для введення хвилин
     onClose(selectedDates) {
-        onDateCheck(selectedDates);
       console.log(selectedDates[0]);
-    },
-  };
-
-  //функція, що викликає метод для форматування занчення(00:00:00)
-  function addLeadingZero(value) {
-    return String(value).padStart(2, '0')
-  };
-  //функція підрахунку (з завдання)
-  function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-     
-    const days = addLeadingZero(Math.floor(ms / day));// Remaining days
-    const hours = addLeadingZero(Math.floor((ms % day) / hour));// Remaining hours
-    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));// Remaining minutes
-    const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));// Remaining seconds
-  
-    return { days, hours, minutes, seconds };
-  };
-
-  //перевірка дати
-  function onDateCheck(selectedDates) {
-    endTime = selectedDates[0].getTime();
-    currentTime = new Date().getTime();
-  
-    if( endTime < currentTime) {
-        window.alert("Please choose a date in the future");
-        startBtn.disable = false;
-        return;
-    } startBtn.disable = true;
-  }; 
-
-  //відлік часу
-  const timer = {
-    start() {
-    const startTimer = Date.now();
-
-    setInterval(() => {
-        const currentTime = Date.now();
-        const ms = endTime - currentTime  ;
-        const time = convertMs;
-        updateTimerFace(time);
-    })
+      selectedDate = selectedDates[0].getTime();
+      currentDate = new Date().getTime();
+//додаєм перевірку валідної дати
+      if( selectedDate < currentDate) {
+        startBtn.disable = true;
+        Notify.failure("Please choose a date in the future");
+      } else {
+       startBtn.disable = false;
+                
+      }
     }
   };
-  //запускаємо таймер по кліку на кнопку старт
-  startBtn.addEventListener('click', () => {
-    timer.start();
-  });
+//функція створення екземпляру дати та часу
+flatpickr(dateInput, options);
+//запускаємо таймер по кліку на кнопку старт
+startBtn.addEventListener('click', timerStart);
+  
+  //таймер відліку часу
+  function timerStart() {
+    let timerId = setInterval(() => {
+      currentDate = new Date().getTime();
+      //dateInput.disable = false;
+      startBtn.disabled = true; 
+      let count = selectedDate - currentDate;
+      console.log(count); 
+         
+      if (count >= 0) {
+        let timeData = convertMs(count);
+          daysIndicator.textContent = timeData.days;
+          hoursIndicator.textContent = timeData.hours;
+          minutesIndicator.textContent = timeData.minutes;
+          secondsIndicator.textContent = timeData.seconds;
+      } 
+       //dateInput.disable = false;
+     }, 1000 );  
+                   
+    };
+ 
+  
+//функція, що викликає метод для форматування занчення(00:00:00)
+function addLeadingZero(value) {
+return String(value).padStart(2, '0')
+};
 
-
-  //функція що оновлює інтерфейс таймеру
-  function updateTimerFace({ days, hours, minutes, seconds }) {
-    timerFace.textContent = '${days}:${hours}:${minutes}:${seconds}';
-  };
+//функція підрахунку (з завдання)
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+   
+  const days = addLeadingZero(Math.floor(ms / day));// Remaining days
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));// Remaining hours
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));// Remaining minutes
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));// Remaining seconds
+  
+  return { days, hours, minutes, seconds };
+};
+  
