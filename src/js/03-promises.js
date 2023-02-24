d38f2119f1d417159b73c94326e85f0d2f7f31c8
Vita <ekokalgan@gmail.com>
2023-02-24 11:36:14 +0200

    03.js-done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,8 +2,27 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const formRef = document.querySelector('.form');
 formRef.addEventListener('click', newCreate);
+ 
+//функція ззавдання
+function createPromise(position, delay) {   
+  const obj = { position, delay };                       //ствроюєм об'єкт який буде значенням промісів з властивостями (position,delay) 
+                    
+  return new Promise((resolve, reject) => {            //обгортаємо функцію в проміс  
+    
+    const shouldResolve = Math.random() > 0.3;           //умова для створення об'єкта 
+      setTimeout(() => {                                //записуємо метод setTimeout() щоб запуск функції відбувся після виконання промісу через delay секунд
+        if (shouldResolve) {
+          resolve(obj);                                  //якщо операція успішна(resolve)передаєм їй номер промісу (position), що створюється,
+                                                          // і затримку, враховуючи першу затримку (delay), введену користувачем, і крок (step).
+        } else {                                          //в разі помилки(reject) те ж саме (щоб resolve, delay повернулись в повідомленні)
+          reject(obj);
+        }
+      },delay );
+  })
+  
+};  
 
- //створюэмо функцію в якій виконується ітерація по amount, вираховується час затримки і викликається  createPromise.
+//створюэмо функцію в якій виконується ітерація по amount, вираховується час затримки і викликається  createPromise.
 function newCreate(e) {
   e.preventDefault();                                     //відміна дії браузера по замовчуванню
   let amount = Number(formRef.amount.value);              //приводимо введенні дані до числа
@@ -13,32 +32,13 @@ function newCreate(e) {
   for (let i = 1; i <= amount; i += 1) {                  
     let timeDelay = delay + step * i;
       createPromise(i, timeDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`); 
+        
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        
+      }) 
     }
 }; 
-
-//функція ззавдання
-function createPromise(position, delay) {   
-  const obj = { position, delay };                       //ствроюєм об'єкт який буде значенням промісів з властивостями (position,delay) 
-                    
-  return new Promise((resolve, reject) => {            //обгортаємо функцію в проміс  
-    
-    const shouldResolve = Math.random() > 0.3;
-      setTimeout(() => {                                //записуємо метод setTimeout() щоб запуск функції був після виконання промісу
-        if (shouldResolve) {
-          resolve(obj);                                  //якщо операція успішна(resolve)передаєм їй номер промісу (position), що створюється,
-                                                          // і затримку, враховуючи першу затримку (delay), введену користувачем, і крок (step).
-        } else {                                          //в разі помилки(reject) те ж саме (щоб resolve, delay повернулись в повідомленні)
-          reject(obj);
-        }
-      },delay );
-  })
-  .then(({ position, delay }) => {
-    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`); 
-    
-  })
-  .catch(({ position, delay }) => {
-    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-    
-  }) 
-};  
-
